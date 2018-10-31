9f832c72ca7ca0feb8922cf6726722c5d23bb53f
Octavian Mirica <Aurelian.Mirica@endava.com>
2018-10-31 17:52:49 +0200

    Advancing slowly but steady

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,54 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+  checkedInDate: number | null;
+}
+
 @Component({
   selector: 'app-root',
   template: `
     <div class="app">
-      <h1>{{ title }}</h1>
-      <div>
-
-      </div>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [ngClass]="{ 'checked-in': passenger.checkedIn }"></span>
+          {{ i }}: {{ passenger.fullname }}
+          <p>{{ passenger | json }}</p>
+        </li>
+      </ul>
     </div>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string;
-
-  constructor() {
-    this.title = 'Ultimate Angular';
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true,
+    checkedInDate: 1490742000000
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false,
+    checkedInDate: null
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true,
+    checkedInDate: 1491602000000
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true,
+    checkedInDate: 1488742000000
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false,
+    checkedInDate: null
+  }];
 }
